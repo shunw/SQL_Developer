281e6a96e0876170a0fb88c15f2d065e8dbd7699
shunw <shunw.li@outlook.com>
2015-04-14 13:07:53 +0800

    Update practice.sql
    Obj: 1. link two tables;/ 2. add filter condition

--- practice.sql
+++ practice.sql
@@ -7,4 +7,5 @@
 select Project.tblTABLE1."Field1", Project.tblTABLE1."Field2", "Field1-in-tbl2"
 from Project.tblTABLE1, Project.tblTABLE2
 where Project.tblTABLE1."key"=Project.tblTABLE2."key"
+  and Project.tblTABLE1."Field2" like '%ABC%'
 order by "any Field";
