40889e3eddfefb1329dfd9d375f4936261bd3a8e
shunw <shunw.li@outlook.com>
2015-04-14 12:39:13 +0800

    Create practice.sql

---
+++ practice.sql
@@ -0,0 +1,10 @@
+--Object: to link two tables
+--Tool: SQL developer
+
+--replace: 
+--Field1 // Field2 // Field1-in-tbl2
+--Project.tblTABLE1 // Project.tblTABLE2
+select Project.tblTABLE1."Field1", Project.tblTABLE1."Field2", "Field1-in-tbl2"
+from Project.tblTABLE1, Project.tblTABLE2
+where Project.tblTABLE1."key"=Project.tblTABLE2."key"
+order by "any Field";
